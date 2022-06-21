449e4b5067be81a6e47110fbd9e5ac4f7bc9f6ec
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-21 16:05:28 +0200

    feat: filter apiData done

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,18 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  let filteredData = [];
+
+  for (let data of apiData) {
+    let containsKeys = true;
+
+    for (let keys of mandatoryKeys) {
+      let comparison = Object.keys(data);
+      if (comparison.includes(keys) === false) {
+        containsKeys = false;
+      }
+    }
+    if (containsKeys == true) {
+      filteredData.push(data);
+    }
+  }
+  return filteredData;
 }
