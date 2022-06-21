ff5a8734f7f3854bfb2f5ae92a5332c27442b3b2
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-21 17:10:54 +0200

    feat: zip-strings first attempt is crap

--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,13 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  let mergedArr = [];
+  const arrStrA = strA.split("");
+  const arrStrB = strB.split("");
+
+  for (let el of arrStrA) {
+    mergedArr.push(el);
+    for (let ol of arrStrB) {
+      mergedArr.push(ol);
+    }
+  }
+  return mergedArr.join();
 }
